d444ebb2084223b6f7f64d7f556393da836ade20
leandrosardi <leandro.sardi@expandedventure.com>
2023-09-28 09:03:56 -0300

    new generic monitoring extension

--- app.rb
+++ app.rb
@@ -3,12 +3,28 @@ get '/monitoring', :auth => true do
     erb :"/extensions/monitoring/views/dashboard", :layout => :'/views/layouts/core'
 end
 
+get '/monitoring/', :auth => true do
+    erb :"/extensions/monitoring/views/dashboard", :layout => :'/views/layouts/core'
+end
+
+get '/monitoring/dashboard', :auth => true do
+    erb :"/extensions/monitoring/views/dashboard", :layout => :'/views/layouts/core'
+end
+
+get '/monitoring/accounts', :auth => true do
+    erb :"/extensions/monitoring/views/accounts", :layout => :'/views/layouts/core'
+end
+
 get '/monitoring/users', :auth => true do
     erb :"/extensions/monitoring/views/users", :layout => :'/views/layouts/core'
 end
 
-get '/monitoring/nodes', :auth => true do
-    erb :"/extensions/monitoring/views/nodes", :layout => :'/views/layouts/core'
+get '/monitoring/engineering', :auth => true do
+    erb :"/extensions/monitoring/views/engineering", :layout => :'/views/layouts/core'
+end
+
+get '/monitoring/financial', :auth => true do
+    erb :"/extensions/monitoring/views/financial", :layout => :'/views/layouts/core'
 end
 
 # filters
@@ -32,20 +48,6 @@ get '/monitoring/filter_allow_trials', :auth => true do
     erb :"/extensions/monitoring/views/filter_allow_trials"
 end
 
-
-# TODO: move this to the agency extension
-get '/monitoring/dashboard', :auth => true do
-    erb :"/extensions/monitoring/views/dashboard", :layout => :'/views/layouts/core'
-end
-
-get '/monitoring/production', :auth => true do
-    erb :"/extensions/monitoring/views/production", :layout => :'/views/layouts/core'
-end
-
-get '/monitoring/financial', :auth => true do
-    erb :"/extensions/monitoring/views/financial", :layout => :'/views/layouts/core'
-end
-
 get '/monitoring/filter_allow_premium', :auth => true do
     erb :"/extensions/monitoring/views/filter_allow_premium"
 end
@@ -56,33 +58,4 @@ end
 
 get '/monitoring/filter_add_bonus', :auth => true do
     erb :"/extensions/monitoring/views/filter_add_bonus"
-end
-
-get '/monitoring/campaigns', :auth => true do
-    erb :"/extensions/monitoring/views/campaigns", :layout => :'/views/layouts/core'
-end
-
-# TODO: move this to the agency extension
-get '/monitoring/scrapers', :auth => true do
-    erb :"/extensions/scraper/views/dashboard", :layout => :'/views/layouts/core'
-end
-
-get '/monitoring/filter_payout', :auth => true do
-    erb :"/extensions/scraper/views/filter_payout"
-end
-
-get '/monitoring/addresses', :auth => true do
-    erb :"/extensions/emails/views/addresses", :layout => :'/views/layouts/core'
-end
-
-get '/monitoring/unibox', :auth => true do
-    erb :"/extensions/emails/views/unibox", :layout => :'/views/layouts/core'
-end
-
-# AJAX
-get '/ajax/monitoring/account_report.json', :auth => true, :agent => /(.*)/ do
-    erb :'/extensions/monitoring/views/ajax/account_report'
-end
-post '/ajax/monitoring/account_report.json', :auth => true, :agent => /(.*)/ do
-    erb :'/extensions/monitoring/views/ajax/account_report'
 end
